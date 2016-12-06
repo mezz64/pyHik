be1f397f3442c23cb6b81ca69f85434b623640a3
mezz64 <jtmihalic@gmail.com>
2016-12-05 21:41:14 -0500

    Incremental test release

--- setup.py
+++ setup.py
@@ -16,12 +16,12 @@ from distutils.core import setup
 setup(
     name='pyHik',
     packages=['pyhik'],
-    version='0.1',
+    version='0.0.1',
     description='Provides a python api to interact with a Hikvision camera event stream.',
     author='John Mihalic',
     author_email='mezz64@users.noreply.github.com',
     url='https://github.com/mezz64/pyhik',
-    download_url='https://github.com/mezz64/pyhik/tarball/0.1',
+    download_url='https://github.com/mezz64/pyhik/tarball/0.0.1',
     keywords=['hikvision', 'event stream', 'api wrapper'],
     classifiers=[],
     )
