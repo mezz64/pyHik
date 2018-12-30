70b88dc15e3e354c29d271c42770f3105434507c
John Mihalic <2854333+mezz64@users.noreply.github.com>
2018-12-30 04:17:41 +0000

    Bump to version 0.1.9

--- pyhik/constants.py
+++ pyhik/constants.py
@@ -2,13 +2,13 @@
 pyhik.constants
 ~~~~~~~~~~~~~~~~~~~~
 Constants list
-Copyright (c) 2016-2018 John Mihalic <https://github.com/mezz64>
+Copyright (c) 2016-2019 John Mihalic <https://github.com/mezz64>
 Licensed under the MIT license.
 """
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 1
-SUB_MINOR_VERSION = 8
+SUB_MINOR_VERSION = 9
 __version__ = '{}.{}.{}'.format(
     MAJOR_VERSION, MINOR_VERSION, SUB_MINOR_VERSION)
 
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016-2018 John Mihalic <https://github.com/mezz64>
+# Copyright (c) 2016-2019 John Mihalic <https://github.com/mezz64>
 # Licensed under the MIT license.
 
 # Used this guide to create module
@@ -16,12 +16,12 @@ from distutils.core import setup
 setup(
     name='pyHik',
     packages=['pyhik'],
-    version='0.1.8',
+    version='0.1.9',
     description='Provides a python api to interact with a Hikvision camera event stream.',
     author='John Mihalic',
     author_email='mezz64@users.noreply.github.com',
     url='https://github.com/mezz64/pyhik',
-    download_url='https://github.com/mezz64/pyhik/tarball/0.1.8',
+    download_url='https://github.com/mezz64/pyhik/tarball/0.1.9',
     keywords=['hik', 'hikvision', 'event stream', 'events', 'api wrapper', 'homeassistant'],
     classifiers=[],
     )
