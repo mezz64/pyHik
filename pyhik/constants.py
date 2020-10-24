c9174844745f3f3411233900eec996712b4b26a5
John Mihalic <2854333+mezz64@users.noreply.github.com>
2020-10-24 19:33:32 -0400

    0.2.8 Release bump

--- pyhik/constants.py
+++ pyhik/constants.py
@@ -8,7 +8,7 @@ Licensed under the MIT license.
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 2
-SUB_MINOR_VERSION = 7
+SUB_MINOR_VERSION = 8
 __version__ = '{}.{}.{}'.format(
     MAJOR_VERSION, MINOR_VERSION, SUB_MINOR_VERSION)
 
