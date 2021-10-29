a52c4152df049d9d51562ee9b4106a37d637907d
mezz64 <2854333+mezz64@users.noreply.github.com>
2021-10-28 20:54:07 -0400

    Version 0.3.0 release (#80)

--- setup.py
+++ setup.py
@@ -16,13 +16,13 @@ from distutils.core import setup
 setup(
     name='pyHik',
     packages=['pyhik'],
-    version='0.2.9',
+    version='0.3.0',
     description='Provides a python api to interact with a Hikvision camera event stream and toggle motion detection.',
     author='John Mihalic',
     author_email='mezz64@users.noreply.github.com',
     license='MIT',
     url='https://github.com/mezz64/pyhik',
-    download_url='https://github.com/mezz64/pyhik/tarball/0.2.9',
+    download_url='https://github.com/mezz64/pyhik/tarball/0.3.0',
     keywords=['hik', 'hikvision', 'event stream', 'events', 'api wrapper', 'homeassistant'],
     classifiers=[],
     )
